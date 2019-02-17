aa77a2b892923813aafe12b6d1c86c862c9e9cc3
Niraj Mehta <niraj86995@gmail.com>
2019-02-17 16:46:30 +0530

    Delete this.js

--- 10. JS -BasicObjects/this.js
+++
@@ -1,9 +0,0 @@
-var comment = {};
-comment.data = ["Nice", "Keep It Up", "Not Good"];
-comment.print = function(){
-	for(var i =0; i<this.data.length; i++) {
-		console.log(this.data[i]);
-	}
-}
-
-comment.print();
