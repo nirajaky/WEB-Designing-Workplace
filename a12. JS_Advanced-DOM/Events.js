ef7ef97a39299727097442cd73607b1e85beffe3
Niraj Mehta <niraj86995@gmail.com>
2019-03-19 20:54:08 +0530

    Add files via upload

---
+++ a12. JS_Advanced-DOM/Events.js
@@ -0,0 +1,20 @@
+// var button = document.querySelector("button");
+// var paragraph = document.querySelector("p");
+
+// button.addEventListener("click", function(){
+// 	document.paragraph.textContent = "Someone has clicked Me";
+// });
+// --------------------------------------------------------
+
+// var ul = document.querySelector("ul");
+// ul.addEventListener("click", function() {
+// 	console.log("You clicked ul");
+// })
+// --------------------------------------------------------
+
+// var lis = document.querySelectorAll("li");
+// for(var i =0; i<lis.length; i++){
+// 	lis[i].addEventListener("click", function(){
+// 		this.style.color = "pink";
+// 	});
+//
