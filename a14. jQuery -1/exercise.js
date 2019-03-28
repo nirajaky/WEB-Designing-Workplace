46e8542e358f28a067a0ef2b270452649c00e464
Niraj Mehta <niraj86995@gmail.com>
2019-03-28 21:12:27 +0530

    Add files via upload

---
+++ a14. jQuery -1/exercise.js
@@ -0,0 +1,7 @@
+$("div").css("background", "purple");
+
+$(".highlight").css("width", "200px");
+
+$("#third").css("border", "5px solid orange");
+
+$("div:nth-of-type(1)").css("color", "pink");
---
+++ a14. jQuery -1/jQueryMethods.js
@@ -0,0 +1,13 @@
+$("li.spl").css("color", "brown");
+
+console.log($("h1").text());
+
+console.log($("h1").text("My Name is Niraj"));
+
+$("h1").text("DEMO PAGE");
+
+$("li").html("<li>Apple</li><li>Ball</li> ");
+
+// $("input").attr("type", "color");
+
+console.log($("input").val("Niraj"));
