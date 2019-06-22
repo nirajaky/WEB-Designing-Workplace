9daad4d460e305b541c477ee54449d7ba9c7c640
Niraj Mehta <niraj86995@gmail.com>
2019-06-22 15:06:05 +0530

    Update App.js

--- a31. Authentication/AuthDemo/App.js
+++ a31. Authentication/AuthDemo/App.js
@@ -31,6 +31,7 @@ app.get("/", function (req, res) {
     res.render("home.ejs");
 });
 
+// MIDDLEWARE ADDED
 app.get("/secret", isLoggedIn, function (req, res) {
     res.render("secret.ejs");
 });
