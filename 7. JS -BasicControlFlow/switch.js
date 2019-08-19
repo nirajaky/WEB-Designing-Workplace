db07864f70262143ba23a8eb88cfd3b8776a82d2
Niraj Mehta <niraj86995@gmail.com>
2019-08-19 23:40:31 +0530

    Update switch.js

--- 7. JS -BasicControlFlow/switch.js
+++ 7. JS -BasicControlFlow/switch.js
@@ -1,1 +1,35 @@
+var tag = document.querySelector("p");
+console.log(tag);
+var day =4;
+
+switch(day){
+	case 1:
+		console.log("Monday");
+		tag.textContent = "Monday";
+		break;
+	case 2:
+		console.log("Tuesday");
+		tag.textContent = "Tuesday";
+		break;
+	case 3:
+		console.log("Wed");
+		tag.textContent = "Wed";
+		break;
+	case 4:
+		console.log("Thr");
+		tag.textContent = "Thr";
+		break;
+	case 5:
+		console.log("Fri");
+		tag.textContent = "Fri";
+		break;
+	case 6:
+		console.log("Sat");
+		tag.textContent = "Sat";
+		break;
+	case 7:
+		console.log("Sun");
+		tag.textContent = "Sun";
+		break;
+}
 
